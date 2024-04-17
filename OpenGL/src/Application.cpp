46ad1fae63deb051c39ac2b3f680c292569f48be
VidaTheHydroxZ <david.vidakovic01@gmail.com>
2024-04-17 23:23:17 +0200

    Added texture rendering and shaders

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,6 +10,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -48,10 +49,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-           -0.5f, -0.5f, // 0
-            0.5f, -0.5f, // 1
-            0.5f,  0.5f, // 2
-           -0.5f,  0.5f  // 3
+           -0.5f, -0.5f, 0.0f, 0.0f, // 0
+            0.5f, -0.5f, 1.0f, 0.0f, // 1
+            0.5f,  0.5f, 1.0f, 1.0f, // 2
+           -0.5f,  0.5f, 0.0f, 1.0f  // 3
         };
 
         unsigned int indices[]{
@@ -59,17 +60,27 @@ int main(void)
             2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
         VertexBufferLayout layout;
+        layout.Push(GL_FLOAT, 2);
         layout.Push(GL_FLOAT, 2);
         va.AddBuffer(vb, layout);
         
         IndexBuffer ib(indices, 6);
-
+        
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+        Texture texture("Cherno.png");
+        texture.Bind();
+
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         shader.Unbind();

354654a20396f968419b71c64c50029e33397a0f
VidaTheHydroxZ <david.vidakovic01@gmail.com>
2024-04-24 22:43:40 +0200

    Added a test button and TestMenu class

--- openGLNew/src/Application.cpp
+++ openGLNew/src/Application.cpp
@@ -56,48 +56,48 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
-    //    float positions[] = {
-    //       -50.0f, -50.0f, 0.0f, 0.0f, // 0
-    //        50.0f, -50.0f, 1.0f, 0.0f, // 1
-    //        50.0f,  50.0f, 1.0f, 1.0f, // 2
-    //       -50.0f,  50.0f, 0.0f, 1.0f  // 3
-    //    };
+        float positions[] = {
+           -50.0f, -50.0f, 0.0f, 0.0f, // 0
+            50.0f, -50.0f, 1.0f, 0.0f, // 1
+            50.0f,  50.0f, 1.0f, 1.0f, // 2
+           -50.0f,  50.0f, 0.0f, 1.0f  // 3
+        };
 
-    //    unsigned int indices[]{
-    //        0, 1, 2,
-    //        2, 3, 0
-    //    };
+        unsigned int indices[]{
+            0, 1, 2,
+            2, 3, 0
+        };
 
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-        //VertexArray va;
-        //VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
-        //VertexBufferLayout layout;
-        //layout.Push(GL_FLOAT, 2);
-        //layout.Push(GL_FLOAT, 2);
-        //va.AddBuffer(vb, layout);
+        VertexBufferLayout layout;
+        layout.Push(GL_FLOAT, 2);
+        layout.Push(GL_FLOAT, 2);
+        va.AddBuffer(vb, layout);
 
-        //IndexBuffer ib(indices, 6);
+        IndexBuffer ib(indices, 6);
 
-        //glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); // this means that projection matrix is between -2.0x and 2.0x and -2.0y and 2.0y (-1 and +1) 
-        //glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-        ////glm::mat4 projection = glm::perspective()
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); // this means that projection matrix is between -2.0x and 2.0x and -2.0y and 2.0y (-1 and +1) 
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+        //glm::mat4 projection = glm::perspective()
 
-        //Shader shader("res/shaders/Basic.shader");
-        //shader.Bind();
-        //
+        /*Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        
 
-        //Texture texture("Cherno.png");
-        //texture.Bind();
+        Texture texture("Cherno.png");
+        texture.Bind();
 
-        //shader.SetUniform1i("u_Texture", 0);
+        shader.SetUniform1i("u_Texture", 0);
 
-        //va.Unbind();
-        //shader.Unbind();
-        //vb.Unbind();
-        //ib.Unbind();
+        va.Unbind();
+        shader.Unbind();
+        vb.Unbind();
+        ib.Unbind();*/
 
         Renderer renderer;
 
@@ -105,48 +105,61 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        test::TestClearColor test;
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
 
- /*       glm::vec3 translationA(200, 200, 0);
+        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+
+
+        /*glm::vec3 translationA(200, 200, 0);
         glm::vec3 translationB(400, 200, 0);*/
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
             renderer.Clear();
 
-            test.OnUpdate(0.0f);
-            test.OnRender();
-
             ImGui_ImplGlfwGL3_NewFrame();
+            if (currentTest)
+            {
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
+            }
 
-            test.OnImGuiRender();
+            /*{
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            //{
-            //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            //    glm::mat4 mvp = proj * view * model;
-            //    shader.Bind();
-            //    shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            //    renderer.Draw(va, ib, shader);
-            //}
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            //{
-            //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            //    glm::mat4 mvp = proj * view * model;
-            //    shader.Bind();
-            //    shader.SetUniformMat4f("u_MVP", mvp);
-
-            //    renderer.Draw(va, ib, shader);
-            //}
-            ////GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
+                renderer.Draw(va, ib, shader);
+            }*/
+            //GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
           
-            //ImGui::Text("Hello, world!");                          
-            //ImGui::SliderFloat3("##button1", &translationA.x, 0.0f, 960.0f);
+            /*ImGui::Text("Hello, world!");                          
+            ImGui::SliderFloat3("##button1", &translationA.x, 0.0f, 960.0f);
 
-            //ImGui::SliderFloat3("##button2", &translationB.x, 0.0f, 960.0f);    
-            //  
+            ImGui::SliderFloat3("##button2", &translationB.x, 0.0f, 960.0f);*/    
+              
             //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
             ImGui::Render();
@@ -156,6 +169,10 @@ int main(void)
 
             glfwPollEvents();
         }
+
+        delete currentTest;
+        if (currentTest != testMenu)
+            delete testMenu;
     }
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();

e3fc61d223950c41aa1fe324ac8a7af7587c9e45
VidaTheHydroxZ <david.vidakovic01@gmail.com>
2024-04-25 23:29:12 +0200

    Added new tests for Texture rendering

--- openGLNew/src/Application.cpp
+++ openGLNew/src/Application.cpp
@@ -19,6 +19,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -110,6 +111,7 @@ int main(void)
         currentTest = testMenu;
 
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
 
         /*glm::vec3 translationA(200, 200, 0);
